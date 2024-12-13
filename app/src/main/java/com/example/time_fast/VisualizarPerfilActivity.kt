9b71fec32d887815928adbf18444360f9e6c8e14
JoseMartine0 <zs19016416@estudiantes.uv.mx>
2024-12-13 17:53:25 -0600

    Modificacion de diseño

---
+++ app/src/main/java/com/example/time_fast/VisualizarPerfilActivity.kt
@@ -0,0 +1,20 @@
+package com.example.time_fast
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class VisualizarPerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_vizualizar_perfil)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.visualizar)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
